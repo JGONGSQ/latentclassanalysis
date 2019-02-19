32a96ba587451a102a0ebe014ebfa1273056bd07
James <james.gong@accesshq.com>
2019-02-20 05:05:46 +1100

    clean up

--- EVlatentAnalysis.py
+++ EVlatentAnalysis.py
@@ -11,49 +11,19 @@ from headers import *
 from loglikelihood import *
 from statistics import *
 
+############ Defined Variables #############################
+VEHICLE_OPTION1_PRICE_SCALED = DefineVariable('VEHICLE_OPTION1_PRICE_SCALED', alt1pr / 10000.0)
+VEHICLE_OPTION2_PRICE_SCALED = DefineVariable('VEHICLE_OPTION2_PRICE_SCALED', alt2pr / 10000.0)
+
+
 ######################################## Class A #############################################
 ASC_OPTION1_A = Beta('ASC_OPTION1_A', 0.5, -10, 10, 0, 'OPTION1 constant;')
-ASC_OPTION2_A = Beta('ASC_OPTION2_A',0.5, -10, 10, 0, 'OPTION2 constant;')
-ASC_OPTION3_A = Beta('ASC_OPTION3_A',0.5, -10, 10, 1,'OPTION3 constant;')
+ASC_OPTION2_A = Beta('ASC_OPTION2_A', 0.5, -10, 10, 0, 'OPTION2 constant;')
+ASC_OPTION3_A = Beta('ASC_OPTION3_A', 0.5, -10, 10, 1,'OPTION3 constant;')
 
-B_VEHICLE_PRICE_A = Beta('B_VEHICLE_PRICE_A',0.5,-10,10,0,'Vehicle Price')
-# B_VEHICLE_PRICE2 = Beta('B_VEHICLE_PRICE2',0,-10,10,0,'Vehicle Price 2')
-# B_VEHICLE_PRICE3 = Beta('B_VEHICLE_PRICE3',0,-10,10,1,'Vehicle Price 3')
-# B_VEHICLE_TYPE = Beta('B_VEHICLE_TYPE',0,-10,10,0,'Vehicle Type')
-# B_VEHICLE_TYPE2 = Beta('B_VEHICLE_TYPE2',0,-10,10,0,'Vehicle Type 2')
-# B_VEHICLE_TYPE3 = Beta('B_VEHICLE_TYPE3',0,-10,10,1,'Vehicle Type 3')
+B_VEHICLE_PRICE_A = Beta('B_VEHICLE_PRICE_A', 0.5, -10, 10, 0, 'Vehicle Price')
 
 # Utility functions
-
-#If the person has a GA (season ticket) her incremental cost is actually 0
-#rather than the cost value gathered from the
-# network data.
-
-# SM_COST =  SM_CO   * (  GA   ==  0  )
-# TRAIN_COST =  TRAIN_CO   * (  GA   ==  0  )
-
-# For numerical reasons, it is good practice to scale the data to
-# that the values of the parameters are around 1.0.
-# A previous estimation with the unscaled data has generated
-# parameters around -0.01 for both cost and time. Therefore, time and
-# cost are multipled my 0.01.
-
-# The following statements are designed to preprocess the data. It is
-# like creating a new columns in the data file. This should be
-# preferred to the statement like
-# TRAIN_TT_SCALED = TRAIN_TT / 100.0
-# which will cause the division to be reevaluated again and again,
-# throuh the iterations. For models taking a long time to estimate, it
-# may make a significant difference.
-
-VEHICLE_OPTION1_PRICE_SCALED = DefineVariable('VEHICLE_OPTION1_PRICE_SCALED', alt1pr / 1000.0)
-VEHICLE_OPTION2_PRICE_SCALED = DefineVariable('VEHICLE_OPTION2_PRICE_SCALED', alt2pr / 1000.0)
-# TRAIN_COST_SCALED = DefineVariable('TRAIN_COST_SCALED', TRAIN_COST / 100)
-# SM_TT_SCALED = DefineVariable('SM_TT_SCALED', SM_TT / 100.0)
-# SM_COST_SCALED = DefineVariable('SM_COST_SCALED', SM_COST / 100)
-# CAR_TT_SCALED = DefineVariable('CAR_TT_SCALED', CAR_TT / 100)
-# CAR_CO_SCALED = DefineVariable('CAR_CO_SCALED', CAR_CO / 100)
-
 V1A = ASC_OPTION1_A + B_VEHICLE_PRICE_A * VEHICLE_OPTION1_PRICE_SCALED
 V2A = ASC_OPTION2_A + B_VEHICLE_PRICE_A * VEHICLE_OPTION2_PRICE_SCALED
 V3A = ASC_OPTION3_A
@@ -87,40 +57,8 @@ ASC_OPTION2_B = Beta('ASC_OPTION2_B', 0.5, -10, 10, 0, 'OPTION2 constant;')
 ASC_OPTION3_B = Beta('ASC_OPTION3_B', 0, -10, 10, 1, 'OPTION3 constant;')
 
 B_VEHICLE_PRICE_B = Beta('B_VEHICLE_PRICE_B', 0.5, -10, 10, 0, 'Vehicle Price')
-# B_VEHICLE_PRICE2 = Beta('B_VEHICLE_PRICE2',0,-10,10,0,'Vehicle Price 2')
-# B_VEHICLE_PRICE3 = Beta('B_VEHICLE_PRICE3',0,-10,10,1,'Vehicle Price 3')
-# B_VEHICLE_TYPE = Beta('B_VEHICLE_TYPE',0,-10,10,0,'Vehicle Type')
-# B_VEHICLE_TYPE2 = Beta('B_VEHICLE_TYPE2',0,-10,10,0,'Vehicle Type 2')
-# B_VEHICLE_TYPE3 = Beta('B_VEHICLE_TYPE3',0,-10,10,1,'Vehicle Type 3')
 
 # Utility functions
-
-# If the person has a GA (season ticket) her incremental cost is actually 0
-# rather than the cost value gathered from the
-# network data.
-
-# For numerical reasons, it is good practice to scale the data to
-# that the values of the parameters are around 1.0.
-# A previous estimation with the unscaled data has generated
-# parameters around -0.01 for both cost and time. Therefore, time and
-# cost are multipled my 0.01.
-
-# The following statements are designed to preprocess the data. It is
-# like creating a new columns in the data file. This should be
-# preferred to the statement like
-# TRAIN_TT_SCALED = TRAIN_TT / 100.0
-# which will cause the division to be reevaluated again and again,
-# throuh the iterations. For models taking a long time to estimate, it
-# may make a significant difference.
-
-# VEHICLE_OPTION1_PRICE_SCALED = DefineVariable('VEHICLE_OPTION1_PRICE_SCALED', alt1pr / 1000.0)
-# VEHICLE_OPTION2_PRICE_SCALED = DefineVariable('VEHICLE_OPTION2_PRICE_SCALED', alt2pr / 1000.0)
-# TRAIN_COST_SCALED = DefineVariable('TRAIN_COST_SCALED', TRAIN_COST / 100)
-# SM_TT_SCALED = DefineVariable('SM_TT_SCALED', SM_TT / 100.0)
-# SM_COST_SCALED = DefineVariable('SM_COST_SCALED', SM_COST / 100)
-# CAR_TT_SCALED = DefineVariable('CAR_TT_SCALED', CAR_TT / 100)
-# CAR_CO_SCALED = DefineVariable('CAR_CO_SCALED', CAR_CO / 100)
-
 V1B = ASC_OPTION1_B + B_VEHICLE_PRICE_B * VEHICLE_OPTION1_PRICE_SCALED
 V2B = ASC_OPTION2_B + B_VEHICLE_PRICE_B * VEHICLE_OPTION2_PRICE_SCALED
 V3B = ASC_OPTION3_B
