6accc193c7d9ed5a3df0d914b12190ce84e3ba89
James <james.gong@accesshq.com>
2019-02-24 13:10:04 +1100

    Refactor

--- latent_discrete_analysis.py
+++ latent_discrete_analysis.py
@@ -4,11 +4,6 @@ from loglikelihood import *
 from statistics import *
 from lib.biologit_gen import problogit_gen, class_membership_gen
 
-
-ASC_OPTION1 = Beta('ASC_OPTION1', 0.5, -10, 10, 0, 'OPTION1 constant;')
-ASC_OPTION2 = Beta('ASC_OPTION2', 0.5, -10, 10, 0, 'OPTION2 constant;')
-ASC_OPTION3 = Beta('ASC_OPTION3', 0.5, -10, 10, 1, 'OPTION3 constant;')
-
 # Assuming the last option is fixed as the base alternative
 number_of_alternatives = 3
 
@@ -23,20 +18,20 @@ class_variables = {
     "B_VEHICLE_PRICE": "alt#num#pr"
 }
 
-
 # class with alternative specific variables
 class_alternative_variables = {
-    # "B_VEHICLE_PRICE": "alt#num#pr"
+    "B_VEHICLE_PRICE": "alt#num#pr"
+}
+
+# the variables relates to the class membership
+sociademographic_variables = {
+    # "TEST_VARIABLE": "alt2co"
 }
 
 scale_coefficient = {
     "alt#num#pr": 10000.0
 }
 
-# the variables relates to the class membership
-sociademographic_variables = {
-
-}
 
 probs = problogit_gen(
     asc_variables, 
@@ -52,7 +47,7 @@ print("This is the probs #############", probs)
 # Define the panel ObsIter String
 panelObsIter = 'panelObsIter'
 
-class_membership = class_membership_gen(list_of_classes, sociademographic_variables, panelObsIter)
+class_membership = class_membership_gen(list_of_classes, sociademographic_variables, panelObsIter, scale_coefficient)
 
 # Define the metaIterator
 metaIterator('personIter', '__dataFile__', 'panelObsIter', 'id')
--- lib/biologit_gen.py
+++ lib/biologit_gen.py
@@ -1,9 +1,67 @@
 from biogeme import *
 from headers import *
 
+LOWER_LIMIT = -10.0
+UPPER_LIMIT = 10.0
 
 
-def problogit_gen(asc_variables, class_variables, class_alternative_variables, scale_coefficient, number_of_alternatives, list_of_classes):
+def adding_variables_to_utility_equation(
+    base_variable, variable_dict, 
+    scale_coefficient, class_item, index, number_of_alternatives, 
+    initial_value, is_fixed, is_indexed):
+
+    utility_equation = base_variable
+
+    if is_indexed:
+        variable_name_index = index
+    else:
+        variable_name_index = ""
+    
+    # class alternative specific variables
+    for k, v in variable_dict.items():
+        print("this is the k v pair", k, v)
+        beta = Beta(
+            '{var_name}_{index}_{class_item}'.format(
+                var_name=k,
+                index=variable_name_index, 
+                class_item=class_item),
+            initial_value, LOWER_LIMIT, UPPER_LIMIT, is_fixed,
+            '{var_name}_{index}_{class_item}'.format(
+                var_name=k,
+                index=variable_name_index,
+                class_item=class_item)
+            )
+
+        if v in scale_coefficient:
+            scale = scale_coefficient[v]
+        else:
+            scale = 1
+
+        if "#num#" in v:
+            variable_name = v.replace("#num#", str(index))
+        else:
+            variable_name = v
+
+        variable = DefineVariable(
+            '{var_name}_SCALED'.format(var_name=variable_name),
+            Variable(variable_name)/scale
+            )
+
+        if index != number_of_alternatives:
+            utility_equation += beta * variable
+
+    return utility_equation
+
+
+
+def problogit_gen(
+    asc_variables, 
+    class_variables, 
+    class_alternative_variables, 
+    scale_coefficient, 
+    number_of_alternatives, 
+    list_of_classes):
+
     probs = list()
     
     for class_item in list_of_classes:
@@ -27,65 +85,37 @@ def problogit_gen(asc_variables, class_variables, class_alternative_variables, s
                     var_name=asc_variables[0], 
                     index=local_index, 
                     class_item=class_item), 
-                initial_value, -10, 10, is_fixed, 
+                initial_value, LOWER_LIMIT, UPPER_LIMIT, is_fixed, 
                 '{var_name}_{index}_{class_item} constant'.format(
                     var_name=asc_variables[0], 
                     index=local_index, 
                     class_item=class_item)
                 )
 
-            # class specific variables
-            for k, v in class_variables.items():
-                beta = Beta(
-                    '{var_name}_{class_item}'.format(
-                        var_name=k, 
-                        class_item=class_item),
-                    initial_value, -10, 10, is_fixed,
-                    '{var_name}_{class_item}'.format(
-                        var_name=k, 
-                        class_item=class_item)
-                    )
-
-                if v in scale_coefficient:
-                    scale = scale_coefficient[v]
-                else:
-                    scale = 1
-                variable_name = v.replace("#num#", str(local_index))
-                variable = DefineVariable(
-                    '{var_name}_SCALED'.format(var_name=variable_name),
-                    Variable(variable_name)/scale
-                    )
-
-                if local_index != number_of_alternatives:
-                    alt_V += beta * variable
+            alt_V = adding_variables_to_utility_equation(
+                base_variable=alt_V,
+                variable_dict=class_variables,
+                scale_coefficient=scale_coefficient,
+                class_item=class_item,
+                index=local_index,
+                number_of_alternatives=number_of_alternatives,
+                initial_value=initial_value,
+                is_fixed=is_fixed,
+                is_indexed=False
+            )
 
             # class alternative specific variables
-            for k, v in class_alternative_variables.items():
-                print("this is the k v pair", k, v)
-                beta = Beta(
-                    '{var_name}_{index}_{class_item}'.format(
-                        var_name=k,
-                        index=local_index, 
-                        class_item=class_item),
-                    initial_value, -10, 10, is_fixed,
-                    '{var_name}_{index}_{class_item}'.format(
-                        var_name=k,
-                        index=local_index,
-                        class_item=class_item)
-                    )
-
-                if v in scale_coefficient:
-                    scale = scale_coefficient[v]
-                else:
-                    scale = 1
-                variable_name = v.replace("#num#", str(local_index))
-                variable = DefineVariable(
-                    '{var_name}_SCALED'.format(var_name=variable_name),
-                    Variable(variable_name)/scale
-                    )
-
-                if local_index != number_of_alternatives:
-                    alt_V += beta * variable
+            alt_V = adding_variables_to_utility_equation(
+                base_variable=alt_V,
+                variable_dict=class_alternative_variables,
+                scale_coefficient=scale_coefficient,
+                class_item=class_item,
+                index=local_index,
+                number_of_alternatives=number_of_alternatives,
+                initial_value=initial_value,
+                is_fixed=is_fixed,
+                is_indexed=True
+            )
 
             print("This is the alternative utility", alt_V)
             
@@ -98,7 +128,7 @@ def problogit_gen(asc_variables, class_variables, class_alternative_variables, s
 
 
 
-def class_membership_gen(list_of_classes, sociademographic_variables, panelObsIter):
+def class_membership_gen(list_of_classes, sociademographic_variables, panelObsIter, scale_coefficient):
     class_variables = list()
     probsClass = list()
     
@@ -114,12 +144,22 @@ def class_membership_gen(list_of_classes, sociademographic_variables, panelObsIt
 
         class_beta = Beta(
             'CLASS_{class_item}'.format(class_item=class_item),
-            initial_value, -10, 10, is_fixed, 
+            initial_value, LOWER_LIMIT, UPPER_LIMIT, is_fixed, 
             'CLASS_{class_item}'.format(class_item=class_item)
             )
 
-        # insert social demographic variables here
-        # class_beta += age + gender....
+        # adding sociademograohic variables to the class membership utility function
+        class_beta = adding_variables_to_utility_equation(
+                base_variable=class_beta,
+                variable_dict=sociademographic_variables,
+                scale_coefficient=scale_coefficient,
+                class_item=class_item,
+                index=local_index,
+                number_of_alternatives=len(list_of_classes),
+                initial_value=initial_value,
+                is_fixed=is_fixed,
+                is_indexed=False
+            )
 
         class_variables.append(class_beta)
     
@@ -130,7 +170,6 @@ def class_membership_gen(list_of_classes, sociademographic_variables, panelObsIt
         else:
             denominator += exp(variable)
 
-    # print("This is the denominator $$$$$$$$$$$$$$", denominator)
 
     for variable in class_variables:
         prob_class = Sum(exp(variable)/denominator, panelObsIter)/Sum(1, panelObsIter)
